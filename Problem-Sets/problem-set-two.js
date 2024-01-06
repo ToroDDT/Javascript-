d77438cf3191630fc145a926c971ed4bdd30a804
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-01-05 22:17:02 -0800

    Create new Problem set

---
+++ Problem-Sets/problem-set-two.js
@@ -0,0 +1,16 @@
+// 1. Two SUm
+
+// create a function that takes two arguements
+// nums
+// target
+// determine which elements add to target number
+// loop through each element
+// choose the first element and last element
+
+// return thier indexes
+// add them to an array
+
+let twoSum = function (nums, target) {
+  let indices = [];
+  for (let k = 0; k < nums.length; k++) {}
+};
