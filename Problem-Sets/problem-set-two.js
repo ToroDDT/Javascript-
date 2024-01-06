f90f89311fe03ed85dd9db1ef2f70f8a165a43cd
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-01-05 22:30:45 -0800

    Remove comments

--- Problem-Sets/problem-set-two.js
+++ Problem-Sets/problem-set-two.js
@@ -13,13 +13,8 @@
 let twoSum = function (nums, target) {
   let indices = [];
   let counter = 0;
-  // for each element create a new iteration
   for (let k = 0; k < nums.length; k++) {
-    // create counter to record index
-
-    // create a for loop that adds each element and determines if equals target
     for (let i = nums.length - 1; i > i; i--) {
-      // Determine if two values equal target
       if (nums[counter] + nums[i] == target) {
         indices.push(i);
         indices.push(nums.indexOf(nums[couter]));
