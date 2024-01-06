929696d6c1052e86f802ae970ce61b3c32500c0c
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-01-06 00:27:33 -0800

    Fix bug of counter resetting to zero

--- Problem-Sets/problem-set-two.js
+++ Problem-Sets/problem-set-two.js
@@ -1,24 +1,19 @@
 // 1. Two SUm
 
-// create a function that takes two arguements
-// nums
-// target
-// determine which elements add to target number
-// loop through each element
-// choose the first element and last element
-
-// return thier indexes
-// add them to an array
-
 let twoSum = function (nums, target) {
   let indices = [];
   let counter = 0;
   for (let k = 0; k < nums.length; k++) {
-    for (let i = nums.length - 1; i > i; i--) {
+    for (let i = nums.length - 1; i > 0; i--) {
       if (nums[counter] + nums[i] == target) {
         indices.push(i);
         indices.push(nums.indexOf(nums[counter]));
-      } else counter++;
+        break;
+      }
     }
+    counter++;
   }
+  return indices;
 };
+
+console.log(twoSum([2, 7, 11, 15], 9));
