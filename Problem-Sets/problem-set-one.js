7afa0f787f4235fb876c27e947dc93af7840aba6
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-01-05 21:37:22 -0800

    Combine substrings into single string

--- Problem-Sets/problem-set-one.js
+++ Problem-Sets/problem-set-one.js
@@ -1,15 +1,17 @@
 // 1. Palindrome Number
 
-// Pseodocode
-// What is a Palindrome
-// Something that is the same spelled backwards or original
-// How DO I determine if its a palindrome?
-// Create an Alorithmn
-// create a function that takes an integer as an arguement
-// create a variable called arrOfNumbers
-// seperate each number or character into individual strings
-// store them in an array
-// reverse the string
-// if the reverse string is equal and the same to original string
-// return the value true
-// else return false
+function determineIfIntegerIsAPalindrome(integer) {
+  // convert integer to string
+  let integerStr = integer.toString();
+  // create An array of substrings
+  let integerArr = integerStr.split("");
+  // reverse elements in the array
+  let reversedArrayElements = integerArr.reverse();
+  // combine substrings into single string
+  let combineIntegers = reversedArrayElements.join("");
+  if (combineIntegers == integer) {
+    return true;
+  } else return false;
+}
+
+console.log(determineIfIntegerIsAPalindrome(203));
