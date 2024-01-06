a44e573812fa52a01167bdf17ccd416fff1a1dc0
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-01-05 20:20:32 -0800

    Create problem set file

---
+++ Problem-Sets/problem-set-one.js
@@ -0,0 +1,15 @@
+// 1. Palindrome Number
+
+// Pseodocode
+// What is a Palindrome
+// Something that is the same spelled backwards or original
+// How DO I determine if its a palindrome?
+// Create an Alorithmn
+// create a function that takes an integer as an arguement
+// create a variable called arrOfNumbers
+// seperate each number or character into individual strings
+// store them in an array
+// reverse the string
+// if the reverse string is equal and the same to original string
+// return the value true
+// else return false
